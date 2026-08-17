dac7089b2774e98424fa202798b01d414cbb351b
Om Ugale <om.s.ugale@gmail.com>
2026-08-17 12:36:35 +0530

    Fix validation error handling

--- src/main/java/com/gomz/festivallineuptracker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gomz/festivallineuptracker/exception/GlobalExceptionHandler.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
 
         Map<String, String> errors = new HashMap<>();
